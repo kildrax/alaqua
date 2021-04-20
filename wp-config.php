aac839660ec2dfa04a13a67feac212675280cb5d
kildrax <jcbest15@hotmail.com>
2021-04-20 13:51:57 -0500

    Se define el tema por defecto a cargar

--- wp-config.php
+++ wp-config.php
@@ -37,6 +37,9 @@ define( 'DB_CHARSET', 'utf8mb4' );
 /** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+/** Default Theme */
+define('WP_DEFAULT_THEME', 'Alaqua');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
